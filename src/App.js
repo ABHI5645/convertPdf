fb438207fd5e3f327d1421fdf6969825aa2f3f19
Abhi <abhi24.8896@gmail.com>
2023-03-16 20:16:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import html2pdf from 'html2pdf.js';
 
 function App() {
+  const handlePdfDownload = () => {
+    const element = document.getElementById('pdf');
+    const options = {
+      filename: 'my_pdf_document.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().set(options).from(element).save();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id="pdf">
+        <h1>My Web Page</h1>
+        <h1>PDF Content</h1>
+        <p>This content will be included in the PDF.</p>
+       
+      </div>
+      <button onClick={handlePdfDownload}>Download PDF</button>
     </div>
   );
 }
